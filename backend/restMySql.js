4f06f671bbfc0ae59ba58a37693585801a90c757
ywais <yuvalwais@gmail.com>
2020-09-20 18:31:56 +0300

    first entry point

--- backend/restMySql.js
+++ backend/restMySql.js
@@ -27,4 +27,20 @@ app.get('/', (req, res) => {
 res.send("Hello World!")
 });
 
+app.get('/top_songs', (req, res) => {
+  mysqlCon.query(`SELECT songs.id, songs.title, songs.length, artists.name AS artist,albums.name AS album, songs.track_number, songs.lyrics, songs.youtube_link, songs.created_at, songs.upload_at
+  FROM sql_music_service.songs
+  LEFT JOIN artists
+  ON songs.artist=artists.id
+  LEFT JOIN albums
+  ON songs.album=albums.id
+  LIMIT 20;`, (error, results, fields) => {
+      if (error) {
+          res.send(err.message);
+          throw error;
+      };
+      res.send(results);
+    });
+});
+
 app.listen(3001);
