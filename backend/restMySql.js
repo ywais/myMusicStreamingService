0b1c1dd18655c6955bfbdf58a064c48172c4b819
ywais <yuvalwais@gmail.com>
2020-09-20 18:37:54 +0300

    fifth entry point

--- backend/restMySql.js
+++ backend/restMySql.js
@@ -85,4 +85,14 @@ app.get('/top_playlists', (req, res) => {
   });
 });
 
+app.get('/song/:id', async (req, res) =>{
+  mysqlCon.query(`SELECT * FROM songs WHERE id=${req.params.id}`, (error, results, fields) => {
+    if (error) {
+      res.send(err.message);
+      throw error;
+    };
+    res.send(results);
+  });
+});
+
 app.listen(3001);
