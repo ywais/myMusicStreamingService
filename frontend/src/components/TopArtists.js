32ce2be2182d3348a89b897c2c68eea4d8d7e529
ywais <yuvalwais@gmail.com>
2020-09-20 20:04:55 +0300

    artists carousel

--- frontend/src/components/TopArtists.js
+++ frontend/src/components/TopArtists.js
@@ -1,11 +1,45 @@
 import React from 'react';
+import ArtistPreview from './ArtistPreview';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
-function TopArtists() {
+function TopArtists(props) {
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 10,
+      slidesToSlide: 9
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 5,
+      slidesToSlide: 4
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+      slidesToSlide: 1
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+      slidesToSlide: 1
+    }
+  };
+
+  const addArtist = (artists) => artists.map((artist) => (
+    <ArtistPreview
+      key={artist.id}
+      name={artist.name}
+      coverImg={artist.coverImg}
+    />
+  ));
+
   return (
-    <div className="TopArtists">
-
-    </div>
+    <Carousel className='carousel artistCarousel' responsive={responsive}>
+        {addArtist(props.artists)}
+    </Carousel>
   );
 }
 
-export default TopArtists;
+export default TopArtists; 
