2b0cb4d2e07a98cb3af8b2c2fb27f64db9d4a50d
ywais <yuvalwais@gmail.com>
2020-09-22 08:39:04 +0300

    song page''s query params

--- frontend/src/components/RcTable.js
+++ frontend/src/components/RcTable.js
@@ -2,22 +2,19 @@ import React from 'react';
 import Table from "rc-table";
 
 function RcTable({songs, type}) {
-  const columns = Object.keys(songs[0]).map(key => ({
-      title: key,
-      dataIndex: key,
-      key: key
-    }));
+  if(songs.length && songs[0].Title !== null) {
+    const columns = Object.keys(songs[0]).map(key => ({
+        title: key,
+        dataIndex: key,
+        key: key
+      }));
 
-  const data = songs.map(song => {
-    let songInfo = Object.keys(song).map((key) => ({
-      [key]: song[key]
-    }));
-    return Object.assign({}, ...songInfo);
-  });
-
-  if(songs[0].Title === null) {
-    return <h4>No songs {type==='artist'?'for':'in'} {type}</h4>;
-  } else {
+    const data = songs.map(song => {
+      let songInfo = Object.keys(song).map((key) => ({
+        [key]: song[key]
+      }));
+      return Object.assign({}, ...songInfo);
+    });
     return (
       <Table
         columns={columns}
@@ -25,6 +22,8 @@ function RcTable({songs, type}) {
         tableLayout="auto"
       />
     );
+  } else {
+    return <h4>No songs {type==='artist'?'for':'in'} {type}</h4>;
   }
 };
 
--- frontend/src/components/Song.js
+++ frontend/src/components/Song.js
@@ -1,42 +1,52 @@
 import React, { useEffect, useState } from 'react';
 import RcTable from "./RcTable";
+import qs from 'qs';
 import axios from 'axios';
 
 function Song(props) {
   const [songData, setSongData] = useState([{}]);
-
+  const paersedQuery = qs.parse(props.location.search, { ignoreQueryPrefix: true });
+console.log(songData);
   useEffect(() => {
     const getSongData = async () => {
-      const { data } = await axios.get(`http://localhost:3001/song/${props.match.params.id}`);
+      const { data } = await axios.get(`http://localhost:3001/song/${props.match.params.id}?${Object.keys(paersedQuery)[0]}=${paersedQuery[Object.keys(paersedQuery)[0]]}`);
       setSongData(data);
     };
     getSongData();
   }, []);
-console.log(props.match);
-console.log(props.location);
-console.log(props.history);
+
   const getEmbed = songProperties => `https://www.youtube.com/embed/${songProperties.youtubeLink?songProperties.youtubeLink.substring(17):''}`
+
+  const songsArray = (songs) => songs.map((song) => (
+    {Title: song.title, Artist: song.artist, Length: song.length}
+  ));
 
   return (
     <div className='full fullSong'>
       <h1>Song {props.match.params.id}</h1>
-      <div className='info songInfo'>
-      <iframe width='560' height='315'
-      src={getEmbed(songData[0])}
-      frameborder='0'
-      allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-      allowfullscreen>
-      </iframe>
-        <div>
-          <h2>{songData[0].title}</h2>
-          <h3>{songData[0].artist}</h3>
-          <p>{songData[0].album} {songData[0].trackNumber}. {songData[0].length}</p>
-          <h4>Lyrics:</h4>
-          <p>
-            {songData[0].lyrics}
-          </p>
+      <section className={songData.length > 1 ? 'flex' : ''}>
+        <div className='info songInfo'>
+          <iframe width='560' height='315'
+          src={getEmbed(songData[0])}
+          frameborder='0'
+          allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+          allowfullscreen>
+          </iframe>
+          <div>
+            <h2>{songData[0].title}</h2>
+            <h3>{songData[0].artist}</h3>
+            <p>{songData[0].album} {songData[0].trackNumber}. {songData[0].length}</p>
+            <h4>Lyrics:</h4>
+            <p>{songData[0].lyrics}</p>
+          </div>
         </div>
-      </div>
+        {songData.length > 1 ?
+          <div className='songsTable'>
+            <h4>More Songs</h4>
+            <RcTable songs={songsArray(songData[1])} type={Object.keys(paersedQuery)[0]}/>
+          </div>
+        : ''}
+      </section>
     </div>
   );
 }
